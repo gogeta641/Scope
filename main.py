4dfc8749475e0a9300064839a3f1be959a1c1bdb
Karthik Ramachandran <karthik.gogeta@gmail.com>
2020-12-13 16:09:33 +0000

    Global Scope

--- main.py
+++ main.py
@@ -17,3 +17,12 @@ def drink_potion():
 
 drink_potion()
 print(potion_strength)
+
+# Global Scope
+player_health = 10
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+
+drink_potion()
+print(player_health)
